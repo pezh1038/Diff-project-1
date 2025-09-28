7623f4c3ecc6b6dcb233ec93f6d2339acc557693
sooosness <65126976+sooosness@users.noreply.github.com>
2025-09-28 13:18:36 -0600

    Update task_set_E.m

--- task_set_E.m
+++ task_set_E.m
@@ -3,19 +3,20 @@ clear;
 close all;
 %% Call rk4
 
-ti = 0;
-tf = 24;
-npts = 250;
-y01 = 65;
-y02 = 95;
-Td = 77;
-kd1 = 0.2;
-kd2 = 2.0;
+ti = 0; % Initial time
+tf = 24; % Final time
+npts = 250; % Amount of points between times
+y01 = 65; % Initial condition
+y02 = 95; % Initial condition
+Td = 77; % Constant(Thermostat set to:)
+kd1 = 0.2; % Equilibrium Constant
+kd2 = 2.0; % Equilibrium Constant
 f1 = @(t, T) kd1 * (Td - T);
 f2 = @(t, T) kd2 * (Td - T);
 
 % Have 4 variations of the equation with y01, y02, kd1, and kd2
-[t1, w1] = rk4(ti, tf, npts, y01, f1);
+% Solve the differential equation with all 4 variations
+[t1, w1] = rk4(ti, tf, npts, y01, f1); 
 [t2, w2] = rk4(ti, tf, npts, y01, f2);
 [t3, w3] = rk4(ti, tf, npts, y02, f1);
 [t4, w4] = rk4(ti, tf, npts, y02, f2);
